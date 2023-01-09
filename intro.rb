d4869a55a3bb979a709c11a59e5ec2a9a1cf75b6
Nata19754 <chavesnatally0419@gmail.com>
2023-01-09 11:04:42 -0800

    completed user intro

---
+++ intro.rb
@@ -0,0 +1,62 @@
+# INTRO
+# Goal is to get out of the jungle alive
+# Be aware of your surroundings, you have 3 lives. 
+# Game begins with the user in a cold wet cave in the jungle,with no food or water
+# Find and use resources to escape the jungle.
+# Snakes, apes, and spiders are your enemy. Everything in the jungle is out to kill you!!!!!
+# The king toad is your FRIEND. He is the key to your survival , use his help along with his toad army to escape the jungle
+# GOOD LUCK!!
+
+# ENEMY (SPIDER)
+#     (
+#                )
+#               (
+#         /\  .-"""-.  /\
+#        //\\/  ,,,  \//\\
+#        |/\| ,;;;;;, |/\|
+#        //\\\;-"""-;///\\
+#       //  \/   .   \/  \\
+#      (| ,-_| \ | / |_-, |)
+#        //`__\.-.-./__`\\
+#       // /.-(() ())-.\ \\
+#      (\ |)   '---'   (| /)
+#       ` (|           |) `
+# jgs     \)           (/
+
+
+
+# ENEMY (APE)
+        #        ."`".
+        # #  .-./ _=_ \.-.
+        # # {  (,(oYo),) }}
+        # # {{ |   "   |} }
+        # # { { \(---)/  }}
+        # # {{  }'-=-'{ } }
+        # # { { }._:_.{  }}
+        # # {{  } -:- { } }
+        #   {_{ }`===`{  _}
+        # # ((((\)     (/))))      
+
+ # ENEMY (SNAKE)
+# ____
+#             / . .\
+# MT          \  ---<
+#              \  /
+#    __________/ /
+# -=:___________/
+
+#  KING TOAD
+#        o  o   o  o
+#        |\/ \^/ \/|
+#        |,-------.|
+#      ,-.(|)   (|),-.
+#      \_*._ ' '_.* _/
+#       /`-.`--' .-'\
+#  ,--./    `---'    \,--.
+#  \   |(  )     (  )|   /
+#  hjw \  | ||       || |  /
+#  `97  \ | /|\     /|\ | /
+#   /  \-._     _,-/  \
+#  //| \\  `---'  // |\\
+# /,-.,-.\       /,-.,-.\
+# o   o   o      o   o    o
